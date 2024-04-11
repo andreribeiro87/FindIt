ead9794522101064ebb68f8e40cabb990622130d
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-11 15:53:38 +0100

    i wanna give up

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,18 @@
 
 const isProd = process.env.NODE_ENV == "production";
 
+// console.log("isProd", process.env);
+
 const nextConfig = {
   basePath: isProd ? "/FindIt" : "",
   distDir: "dist",
   images: {
+    path: "/FindIt",
+
     unoptimized: true, // FIXME see here
   },
+
+  reactStrictMode: true,
 };
 
 export default nextConfig;

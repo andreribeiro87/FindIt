5a655313bf37875c7feb43fa7d27d345cf8c329d
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-11 14:16:27 +0100

    another another try

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig = {
   basePath: isProd ? "/FindIt" : "",
-  output: "export",
   distDir: "dist",
   images: {
     unoptimized: true, // FIXME see here

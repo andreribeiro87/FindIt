842e62d0b252089997c2ca7e68f752dd8c1a6352
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-11 14:07:07 +0100

    another try

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig = {
   basePath: isProd ? "/FindIt" : "",

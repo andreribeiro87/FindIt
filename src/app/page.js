93d069ceb705669b0d01ac074622a756125f3efa
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-15 09:28:54 +0100

    resolving

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,11 @@
-
-import Cartao from "../../components/Cartao"
+import Cartao from "../../components/Cartao";
 
 export default function Home() {
-<<<<<<< HEAD
-  console.log("isProd", process);
-
-
-=======
->>>>>>> ead9794522101064ebb68f8e40cabb990622130d
   return (
     <>
       <Cartao />
-      
-      <h1>Home</h1>   
+
+      <h1>Home</h1>
     </>
   );
 }

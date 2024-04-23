39d0386877698cea23f4fc424b77378c1f0d0519
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-23 17:31:17 +0100

    json server doing shut

--- src/app/api/serchProdByName/route.js
+++ src/app/api/serchProdByName/route.js
@@ -1,8 +1,8 @@
-export async function GET() {
-  const res = await fetch("localhost:3000/produto", {
-    next: { revalidate: 60 }, // Revalidate every 60 seconds
-  });
-  const data = await res.json();
+// export async function GET() {
+//   const res = await fetch("localhost:3000/produto", {
+//     next: { revalidate: 60 }, // Revalidate every 60 seconds
+//   });
+//   const data = await res.json();
 
-  return Response.json(data);
-}
+//   return Response.json(data);
+// }

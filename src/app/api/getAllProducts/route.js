0d3b63e7d4635672fcafc2c0f764acffd0f8c4e7
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-23 15:18:18 +0100

    api things

---
+++ src/app/api/getAllProducts/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  const res = await fetch("localhost:3000/produto").then((res) => res.json());
+
+  return NextResponse.json({
+    res,
+  });
+}
+
+/**
+ * export async function GET(req) {
+    //logica de id
+    const myURl = new URL(req.url);
+    let id = myURl.searchParams.get('id') || 1;
+    console.log('cheguei')
+    let datum = []
+    // let id = 1;
+    let res1
+
+    while (true) {
+        res1 = await fetch(`http://127.0.0.1:8000/experiencias/${id}`).then(res => res.json())
+        datum.push(res1)
+        if ((myURl.searchParams.get('id') != undefined)) {
+
+            break
+        }
+
+        id++
+
+        if (res1 == null || res1 == undefined || res1 == '' || res1 == 'undefined' || res1 == []) {
+            break
+        }
+
+    }
+    return NextResponse.json({
+        datum
+    })
+
+
+
+
+ */
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import Cartao from "../../components/Cartao";
 import Filter from "../../components/Filter";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/getAllProducts", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       {/* <Cartao /> */}
-      <Filter/>
+
+      <Filter />
       <h1>Home</h1>
     </>
   );

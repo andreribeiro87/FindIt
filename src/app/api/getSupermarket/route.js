fb59df0cbf4f73db34c2a96a6e8ae8e5eff1e01f
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-25 00:47:58 +0100

    shit json server is fucking me again

--- src/app/api/getSupermarket/route.js
+++ src/app/api/getSupermarket/route.js
@@ -1,24 +1,25 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  //   let myurl = new URL(req.url);
-  //   let accessibility = myurl.searchParams.get("accessibility") || "";
+  let myurl = new URL(req.url);
+  let accessibility = myurl.searchParams.get("accessibility") || "false";
+  console.log(myurl.searchParams.get("accessibility"), "PILAO", accessibility);
 
   const res = await fetch("http://127.0.0.1:3001/supermercado").then((res) =>
     res.json()
   );
 
   //   //   console.log(res, accessibility, "PILAO");
-  //   if (accessibility == "true") {
-  //     let res2 = [];
-  //     for (let i = 0; i < res.length; i++) {
-  //       if (res[i].acessibilidade.toString() == accessibility) {
-  //         res2.push(res[i]);
-  //       }
-  //     }
-  //     console.log(res2, "MYDICKCKCK");
-  //     return NextResponse.json(res2);
-  //   }
+  if (accessibility == "true") {
+    let res2 = [];
+    for (let i = 0; i < res.length; i++) {
+      if (res[i].acessibilidade.toString() == accessibility) {
+        res2.push(res[i]);
+      }
+    }
+    console.log(res2, "MYDICKCKCK");
+    return NextResponse.json(res2);
+  }
 
   return NextResponse.json(res);
 }

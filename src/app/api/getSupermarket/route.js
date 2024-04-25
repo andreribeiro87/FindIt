97a11da118484ef76ef941b6fadeeb199602df65
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-25 23:21:26 +0100

    another one

--- src/app/api/getSupermarket/route.js
+++ src/app/api/getSupermarket/route.js
@@ -4,7 +4,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function GET(req) {
   // if req is a promise lets wait from it
   await req;
-  console.log("PIXA1\n", req, "PIXA2\n", "DICA");
+  console.log("PIXA1\n", req, "PIXA2\n", req.nextUrl, "DICA");
 
   // let myurl = new URL(req.url);
   //   let accessibility = myurl.searchParams.get("accessibility") || "false";

17574b729ea08022ae2e14733499fcf5ee6eb49a
André Ribeiro <andrepedro2004@hotmail.com>
2024-04-25 22:46:12 +0100

    nope infini

--- src/app/api/getSupermarket/route.js
+++ src/app/api/getSupermarket/route.js
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req) {
   // console.log("PIXA1\n", req, "PIXA2\n", req.nextUrl.searchParams, "DICA");
-  console.log(req);
+  console.log(req.headers);
 
   // let myurl = new URL(req.url);
   //   let accessibility = myurl.searchParams.get("accessibility") || "false";
